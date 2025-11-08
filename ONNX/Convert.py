6ab17fdfc5f7431439ff4b3bf29b7ca4ed90c996
Moondongoh <mdo0421@naver.com>
2025-11-08 18:26:34 +0900

    ONNX 버전 11로 변환

--- ONNX/Convert.py
+++ ONNX/Convert.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO(r"C:\GIT\Capstone\Web\flask-template\weights\best.pt")
 model.export(format="onnx", opset=11)
-print("Model has been successfully converted to ONNX format..")
+print("Model has been successfully converted to ONNX format.")
